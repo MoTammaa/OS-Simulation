46e48fd86c9d062486a24d97470fd07b97d84817
M-Bahy <Homelander.SolderBoy@gmail.com>
2023-05-19 00:27:18 +0200

    Added FileHandler and Done readFromDisk in Kernel

---
+++ src/os/FileHandler.java
@@ -0,0 +1,8 @@
+package os;
+
+public class FileHandler {
+    
+    /*12. ‘FileHandler’:
+a. Handles file operations like reading and writing data to files.
+b. Interfaces with the operating system's file system. */
+}
--- src/os/Kernel.java
+++ src/os/Kernel.java
@@ -1,10 +1,56 @@
 package os;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.Buffer;
+
 public class Kernel {
+	/*1. ‘Kernel’:
+	/*Functions:
+1. readFromDisk(String filename); Read the data of any file from the disk.
+2.writeToDisk(String text, String filename); Write text output to a file in the disk.
+3. print(Object obj); Print data on the screen. //integer or string or anything
+4. input(); Take text input from the user.
+5. ??: Reading data from memory.
+6. ??: Writing data to memory.
+//not sure of 5,6 */
+    public static String readFromDisk(String filename) {
+    try {
+        BufferedReader br = new BufferedReader(new FileReader(filename));
+        StringBuilder sb = new StringBuilder();
+        String line;
+        while ((line = br.readLine()) != null) {
+            sb.append(line);
+            sb.append(System.lineSeparator());
+        }
+        return sb.toString();
+    } catch (IOException e) {
+        e.printStackTrace();
+        return null;
+    }
+}
+
+    public static void writeToDisk(String text, String filename) {
+    }
+
+    public static void print(Object obj) {
+    }
+
+    public static String input() {
+        return null;
+    }
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
+    String filename = "Program_1.txt";
+    String contents = readFromDisk(filename);
+    if (contents != null) {
+        System.out.println("Contents of " + filename + ":");
+        System.out.println(contents);
+    } else {
+        System.out.println("Failed to read " + filename);
+    }
+}
 
 }
