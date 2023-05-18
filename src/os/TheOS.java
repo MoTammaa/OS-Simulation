a27409ef86eb841dc1337b69cb45ff0d42e46301
M-Bahy <Homelander.SolderBoy@gmail.com>
2023-05-19 01:36:28 +0200

    Added TheOS.java that holds the memory

---
+++ src/os/TheOS.java
@@ -0,0 +1,9 @@
+package os;
+
+public class TheOS {
+    static String [][] memory = new String[40][2]; 
+    // 40 rows, 2 columns
+    // 40 rows for 40 memory locations
+    // 2 columns for Variable name and it’s corresponding data
+    
+}
